2030ca1acf48d0292b7f45109c808d24fe3bac52
keithpohl82 <keith.pohl82@gmail.com>
2023-12-29 10:23:28 -0600

    Simplified Adding tabs to auth modal.

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs-container',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.css'
 })
 export class TabsContainerComponent {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList()
 
 }
