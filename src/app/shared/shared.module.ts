c5f7642506346190b9acd84fc2ef9b3b236aa09a
keithpohl82 <keith.pohl82@gmail.com>
2023-12-29 08:39:40 -0600

    Added Tab Container and Tab component for Auth modal.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 //import { ModalService } from '../services/modal.service';
 
 
@@ -8,7 +10,7 @@ import { ModalComponent } from './modal/modal.component';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, ModalComponent,
+    CommonModule, ModalComponent, TabsContainerComponent, TabComponent
   ],
   exports: [ModalComponent],
   //providers: [ModalService]
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalComponent } from '../../shared/modal/modal.component';
 import { ModalService } from '../../services/modal.service';
+import { TabComponent } from '../../shared/tab/tab.component';
+import { TabsContainerComponent } from '../../shared/tabs-container/tabs-container.component';
 
 @Component({
   selector: 'app-auth-modal',
   standalone: true,
-  imports: [ModalComponent],
+  imports: [ModalComponent, TabComponent, TabsContainerComponent],
   templateUrl: './auth-modal.component.html',
   styleUrl: './auth-modal.component.css'
 })
