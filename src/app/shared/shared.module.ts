bf4025e34d32aae55c9bbb59a8f9a8829cbade46
keithpohl82 <keith.pohl82@gmail.com>
2024-01-04 11:05:36 -0600

    Made Input Component and fixed input showing error if needed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 //import { ModalService } from '../services/modal.service';
 
 
@@ -10,7 +12,7 @@ import { TabComponent } from './tab/tab.component';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, ModalComponent, TabsContainerComponent, TabComponent
+    CommonModule, ModalComponent, TabsContainerComponent, TabComponent, InputComponent, ReactiveFormsModule
   ],
   exports: [ModalComponent],
   //providers: [ModalService]
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,23 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  name = new FormControl('', [Validators.required, Validators.minLength(3)])
+  email = new FormControl('')
+  password =  new FormControl('')
+  confirm_password = new FormControl('')
+  phoneNumber = new FormControl('')
+
   registerForm = new FormGroup({
 
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl(''),
-    AggregateErrorpassword: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: this.name,
+    email: this.email,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+
   })
 
 }
