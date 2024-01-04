7d05cf320423e4eecbdafe18ef8502938b597f10
keithpohl82 <keith.pohl82@gmail.com>
2024-01-04 15:31:52 -0600

    Added Password validators.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -15,9 +15,9 @@ export class RegisterComponent {
   name = new FormControl('', [Validators.required, Validators.minLength(3)])
   email = new FormControl('', [Validators.required, Validators.email])
   age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(125)])
-  password =  new FormControl('')
-  confirm_password = new FormControl('')
-  phoneNumber = new FormControl('')
+  password = new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)])
+  confirm_password = new FormControl('', [Validators.required])
+  phoneNumber = new FormControl('', [Validators.required])
 
   registerForm = new FormGroup({
 
