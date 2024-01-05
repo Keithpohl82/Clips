7cb3f61155e7fe085a5041fa9d33729ba50e7325
keithpohl82 <keith.pohl82@gmail.com>
2024-01-05 11:20:20 -0600

    Finished login template.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  credentials = {
+    email: '',
+    password: ''
+  }
+
+  login() {
+    console.log(this.credentials)
+  }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule, AuthModalComponent, SharedModule, ReactiveFormsModule
+    CommonModule, AuthModalComponent, SharedModule, ReactiveFormsModule, FormsModule
   ],
   exports: [AuthModalComponent]
 })
